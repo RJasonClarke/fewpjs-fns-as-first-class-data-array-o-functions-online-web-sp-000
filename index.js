74ecf5135f6ba9321c1e0c3a84aea9d8f462c564
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-31 20:12:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
+function leashDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
+function walkToPark(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
+function throwFrisbee(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
+function walkHome(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function wakeDog(dogName, dogBreed) {
+function unleashDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
