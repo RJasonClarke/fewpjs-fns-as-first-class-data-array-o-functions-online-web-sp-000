ae9db9e1d073198a0199e50c2cbcc40f78482945
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-31 20:16:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,5 @@ function walkHome(dogName, dogBreed) {
 function unleashDog(dogName, dogBreed) {
   return (`Unleash ${dogName} the ${dogBreed}`);
 }
+
+var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
